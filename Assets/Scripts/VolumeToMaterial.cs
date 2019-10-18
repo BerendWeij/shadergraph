74166e2ac32e209b211a0ea644e3477d6e06ba45
Berend Weij <berend@studiozoetekauw.nl>
2019-10-18 13:25:00 +0200

    adds normal map

---
+++ Assets/Scripts/VolumeToMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeToMaterial : MonoBehaviour
+{
+    [SerializeField] private string propertyName;
+    
+    [SerializeField] private Material targetMaterial;
+
+    void Update()
+    {
+        if (targetMaterial == null)
+        {
+            return;
+        }
+        
+        targetMaterial.SetFloat(propertyName, Random.Range(0f, 2f));
+    }
+}
